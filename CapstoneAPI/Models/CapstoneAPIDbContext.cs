f3ef79296459a12727ef4e25aba6bbf42cf5add4
abhay-sachan <abhay.sachan@tavant.com>
2022-10-31 21:28:31 +0530

    API Complete

--- CapstoneAPI/Models/CapstoneAPIDbContext.cs
+++ CapstoneAPI/Models/CapstoneAPIDbContext.cs
@@ -13,18 +13,18 @@ namespace CapstoneAPI.Models
 
         public DbSet<Product> Product { get; set; }
 
-        //public CapstoneAPIDbContext() : base() { }
+        public CapstoneAPIDbContext() : base() { }
 
-        //public CapstoneAPIDbContext(DbContextOptions<CapstoneAPIDbContext> options) : base(options) { }
+        public CapstoneAPIDbContext(DbContextOptions<CapstoneAPIDbContext> options) : base(options) { }
 
 
-        protected override void OnConfiguring(DbContextOptionsBuilder builder)
-        {
-            if (!builder.IsConfigured)
-            {
-                builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Capstone_DB;Integrated Security=True;");
+        //protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        //{
+        //    if (!builder.IsConfigured)
+        //    {
+        //        builder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Capstone_DB;Integrated Security=True;");
 
-            }
-        }
+        //    }
+        //}
     }
 }
--- CapstoneAPI/Program.cs
+++ CapstoneAPI/Program.cs
@@ -8,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CapstoneAPIDbContext>(x =>
+x.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
+builder.Services.AddCors(c => c.AddPolicy("CapstoneProject", c => c.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader()));
+builder.Services.AddScoped<IAuthenticate, AuthenticateImpl>();
+builder.Services.AddScoped<ICustomer, CustomerImpl>();
+builder.Services.AddScoped<ISeller, SellerImpl>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,14 +22,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-builder.Services.AddDbContext<CapstoneAPIDbContext>(x =>
-x.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
-builder.Services.AddCors(c => c.AddPolicy("CapstoneProject", c => c.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader()));
+
+
 builder.Services.AddEndpointsApiExplorer();
+
 // Configure the HTTP request pipeline.
-builder.Services.AddScoped<IAuthenticate, AuthenticateImpl>();
-builder.Services.AddScoped<ICustomer, CustomerImpl>();
-builder.Services.AddScoped<ISeller, SellerImpl>();
+
+
 app.UseAuthorization();
 app.UseRouting();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
